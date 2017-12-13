e78415d697f51a54fb9c2765acfcc70814fdc231
Genta Kamitani <oftn.mofumofu@gmail.com>
2017-12-13 16:24:00 +0900

    モデルと画像読み込み部分を追加

---
+++ instafly/models.py
@@ -0,0 +1,33 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.applications.vgg16 import VGG16
+
+NUM_HIDDEN = 16
+NUM_CHANNELS = 3
+DEFAULT_ACTIVATION='relu'
+
+def create_model(input_shape):
+    width, height = input_shape
+    model = Sequential()
+    vgg16 = VGG16(
+        input_shape=(width, height, NUM_CHANNELS),
+        include_top=False,
+        weights='imagenet'
+    )
+    for layer in vgg16.layers[:-1]:
+        # 最も出力に近い層のみ学習させる
+        layer.trainable = False
+    model.add(vgg16)
+    model.add(Activation(DEFAULT_ACTIVATION))
+    model.add(Flatten())
+    model.add(Dense(NUM_HIDDEN))
+    model.add(Activation(DEFAULT_ACTIVATION))
+    model.add(Dense(1))
+    model.add(Activation('relu')) # 出力は0以上なので
+
+    model.compile(
+        loss='mse',
+        optimizer='sgd'
+    )
+
+    return model
---
+++ instafly/resources.py
@@ -0,0 +1,36 @@
+import os
+import csv
+from keras.preprocessing import image
+import numpy as np
+
+class Metadata(object):
+    def __init__(self, path, like_rate):
+        self.path = path
+        self.like_rate = like_rate
+
+def flow_metadata(csv_path):
+    with open(csv_path) as f:
+        reader = csv.reader(f)
+        headers = None
+        for row in reader:
+            if headers is None:
+                # 最初の行はヘッダ
+                headers = row
+                continue
+            row_dict = { k: v for k, v in zip(headers, row) }
+            yield Metadata(
+                path=row_dict['path'],
+                like_rate=row_dict['like_rate']
+            )
+
+def load_images(csv_path, img_base_dir, img_shape):
+    X = []
+    Y = []
+    for meta in flow_metadata(csv_path):
+        path = os.path.join(img_base_dir, meta.path)
+        img = image.load_img(path, target_size=img_shape)
+        img = image.img_to_array(img).astype('float32')
+        img /= 255
+        X.append(img)
+        Y.append(meta.like_rate)
+    return np.array(X), np.array(Y).astype('float32')
