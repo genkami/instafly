7e7a2bb4292e2b7133c15650ad05999b8423706e
Genta Kamitani <gkamitani@iip.ist.i.kyoto-u.ac.jp>
2017-12-20 20:01:08 +0900

    モメンタムで最適化するようにした

--- visualize.py
+++ visualize.py
@@ -4,11 +4,34 @@ import keras.backend as K
 import numpy as np
 from keras.preprocessing.image import array_to_img
 from keras.models import load_model
+from keras.optimizers import SGD
 
 from instafly import models
 from instafly import config
 
+class MomentumUpdator(object):
+    def __init__(self, lr=0.01, gamma=0.01):
+        self._lr = lr
+        self._gamma = gamma
+        self._v = None
+
+    def update(self, param, loss, X):
+        grads = K.gradients(loss, [param])[0]
+        f = K.function([param], [loss, grads])
+
+        if self._v is None:
+            self._v = np.zeros(X.shape)
+
+        loss_val, grads_val = f([X])
+        self._v *= self._gamma
+        self._v += self._lr * grads_val
+        X -= self._v
+        return loss_val
+
 class Minimizer(object, metaclass=abc.ABCMeta):
+    def __init__(self, updator=MomentumUpdator(), **kwargs):
+        self._updator = updator
+
     @abc.abstractmethod
     def get_input(self):
         '''
@@ -33,24 +56,22 @@ class Minimizer(object, metaclass=abc.ABCMeta):
         '''
         ...
 
-    def minimize_loss(self, lr=1, num_loops=5000):
+    def minimize_loss(self, num_loops=1000):
         X = self.create_initial_input()
         input = self.get_input()
         loss = self.get_loss()
-        grads = lr * K.gradients(loss, input)[0]
-        func = K.function([input], [loss, grads])
 
         for i in range(num_loops):
-            loss_value, grads_value = func([X])
-            print('%d: %f' % (i, loss_value))
-            X -= grads_value
+            loss_val = self._updator.update(input, loss, X)
+            print('%d: %f' % (i, loss_val))
 
         return X
 
 
 class ImageVisualizer(Minimizer):
-    def __init__(self, model):
+    def __init__(self, model, **kwargs):
         self._model = model
+        super().__init__(**kwargs)
 
     def get_input(self):
         return self._model.input
@@ -64,8 +85,11 @@ class ImageVisualizer(Minimizer):
 
 def main():
     model = load_model('../instafly-resources/weights.h5')
-    visualizer = ImageVisualizer(model)
-    img = visualizer.minimize_loss()[0]
+    visualizer = ImageVisualizer(
+        model,
+        updator=MomentumUpdator(lr=10)
+    )
+    img = visualizer.minimize_loss(num_loops=500)[0]
     img = array_to_img(img)
     img.save('output.png')
 
